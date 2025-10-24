9e2fbec62a1c8ff14bb1c05be92dcfb8a497c654
Musharraf1128 <shahmusharaf1128@gmail.com>
2025-10-24 22:51:00 +0530

    fixed api.js

--- client/src/utils/api.js
+++ client/src/utils/api.js
@@ -1,7 +1,9 @@
 import axios from 'axios';
 
+console.log(import.meta.env.VITE_API_BASE_URL)
+
 const API = axios.create({
-  baseURL: import.meta.env.VITE_API_BASE_URL,
+  baseURL: import.meta.env.VITE_API_BASE_URL, 
 });
 
 // Add token to requests automatically
